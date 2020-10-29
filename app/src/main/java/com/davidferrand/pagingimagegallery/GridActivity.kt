e9a782c73995c5915da0dc465a454d4b2dc63136
David Ferrand <d.ferrand@emogi.com>
2020-10-29 17:38:09 +0100

    Add RecyclerView v4 (without padding and initial positioning)

--- app/src/main/java/com/davidferrand/pagingimagegallery/GridActivity.kt
+++ app/src/main/java/com/davidferrand/pagingimagegallery/GridActivity.kt
@@ -33,6 +33,10 @@ class GridActivity : AppCompatActivity() {
             com.davidferrand.pagingimagegallery.recyclerview.v5.CarouselActivity::class.java
         ),
         CarouselImplementation(
+            "RecyclerView v4",
+            com.davidferrand.pagingimagegallery.recyclerview.v4.CarouselActivity::class.java
+        ),
+        CarouselImplementation(
             "ViewPager v1",
             com.davidferrand.pagingimagegallery.viewpager.v1.CarouselActivity::class.java
         ),
